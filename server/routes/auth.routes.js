f7dda76b51d3b05c78f7d15c741ce0f39d7b6cde
Michael Wohl <mike@gatorriders.com>
2016-03-30 19:11:20 -0400

    Created registration route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -7,6 +7,19 @@ var User = require('../models/user.model.js');
 
 router.post('/register', function(req, res) {
   //registration route for signing up users
+  User.register(new User({username: req.body.username}),
+    req.body.password, function(err, account){
+      if(err){
+        return res.status(500).json({
+          err: err
+        });
+      }
+      passport.authenticate('local')(req, res, function(){
+        return res.status(200).json({
+          status: 'Registration successful'
+        });
+      })
+    });
 });
 
 router.post('/login', function(req, res, next) {
