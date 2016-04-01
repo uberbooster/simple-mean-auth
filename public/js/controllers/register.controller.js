89e46bcc3d4fd61238d003f5b4288aa15a42c1bd
UberBooster <info@uberbooster.com>
2016-04-01 19:51:07 -0400

    register controller functionality complete

--- public/js/controllers/register.controller.js
+++ public/js/controllers/register.controller.js
@@ -5,5 +5,23 @@ RegisterController.$inject = ['$scope', '$location', 'AuthService'];
 
 function RegisterController($scope, $location, AuthService) {
 
-    
+    $scope.register = function(){
+      //intitial values
+      $scope.error = false;
+      $scope.disabled = false;
+
+      //
+      AuthService.register($scope.registerForm.username, $scope.registerForm.password)
+                  .then(function(response){
+                    $location.path('/login');
+                    $scope.disabled = false;
+                    $scope.registerForm = {};
+                  })
+                  .catch(function(){
+                    $scope.error = true;
+                    $scope.errorMessage = 'Something went wrong';
+                    $scope.disabled = false;
+                    $scope.registerForm = {};
+                  });
+    };
 };
